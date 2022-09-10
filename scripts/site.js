1e318147bc4192a57639d0abdab57d0713f211b4
chiencongvn <chiencongvn@hotmail.com>
2022-09-10 16:10:17 +0700

    first commit

---
+++ scripts/site.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var tz = moment.tz.guess(),
+    date   = moment.tz('2022-09-20', tz),
+    now    = new Date(),
+    diff   = (date.valueOf() / 1000) - (now.getTime() / 1000);
+
+    if(diff < 0) {
+        diff = 0;
+    }
+
+    var clock = $('.countdown').FlipClock(diff, {
+        clockFace: 'DailyCounter',
+        countdown: true,
+        showSeconds: true
+    });
+
+});
